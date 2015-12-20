d4cf6564f48cbd09b1a4586ddbb5fd24b016c128
user1 <mahi@sathya.com>
2015-12-20 18:45:49 +0530

    hai this one

---
+++ Demo.java
@@ -0,0 +1,9 @@
+class Demo
+{
+   public static void main(String args[])
+     {
+        int[] a={10,20,2,603,10};
+        Array.sort(a);
+        System.out.println(c);
+      }
+}                        
